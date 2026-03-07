b35707b138ec35b0c8fd625c0ac171f74aa8799f
Faakhir <syedfaakhirabid@gmail.com>
2026-03-07 07:59:22 +0500

    rag_pipeline updated till qdrant

--- rag.py
+++ rag.py
@@ -1,6 +1,11 @@
-from python_dotenv import load_dotenv
+from dotenv import load_dotenv
 from googleapiclient.discovery import build
+import os
 
 load_dotenv()
 
-youtube = build('youtube', 'v3')
+youtube = build('youtube', 'v3' , developerKey=os.getenv("YOUTUBE_API_KEY"))
+
+query = "supabase auth python"
+
+
